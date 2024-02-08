91dfc93ae9a7a1e160c5031930dd2b178f2ba2e5
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-08 15:08:26 +0100

    ts

---
+++ 4-TS-Exemplified.ts
@@ -0,0 +1,8 @@
+// let | const
+let grade: number = 95;
+// grade = "ninety-five";
+
+// type 'string' is not assignable to type 'number'
+// => Type Error
+
+// you cannot change type of any variable in typescript !!
