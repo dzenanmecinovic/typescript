b742f38739a3c15e4a120fef7df9e0c2c0b01014
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-11 22:13:10 +0100

    never type

---
+++ dist/17-never-type.js
@@ -0,0 +1,7 @@
+"use strict";
+function chatFeed() {
+    while (true) {
+    }
+}
+chatFeed();
+console.log("Chat Disconnected");
---
+++ src/17-never-type.ts
@@ -0,0 +1,11 @@
+// never type basically says that the value will never occur.
+
+function chatFeed(): never {
+  while (true) {
+    // Grab the messages and display them on the screen.
+  }
+}
+
+chatFeed();
+
+console.log("Chat Disconnected");
