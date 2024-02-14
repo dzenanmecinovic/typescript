2f57e8eed2f2657fac21df67e7ada3e7ddf696ce
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-14 12:37:37 +0100

    literal types

--- dist/24-literal-types.js
+++ dist/24-literal-types.js
@@ -3,14 +3,13 @@ let myNumber = 7;
 let myString = "Dzenan";
 let courseDiscounts = 35;
 let tip2 = (billAmount, tipPercentage) => {
-    if (typeof tipPercentage === "number") {
-        return billAmount * tipPercentage;
-    }
-    else {
-        let calculatedTip = billAmount * (parseInt(tipPercentage) / 100);
-        return calculatedTip;
-    }
+  if (typeof tipPercentage === "number") {
+    return billAmount * tipPercentage;
+  } else {
+    let calculatedTip = billAmount * (parseInt(tipPercentage) / 100);
+    return calculatedTip;
+  }
 };
-let tipAmount2 = tip(100, 0.2);
+let tipAmount2 = tip2(100, 0.2);
 console.log(tipAmount2);
 let frontendPathIncludes = "Angular";
