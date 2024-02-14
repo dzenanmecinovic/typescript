22db0828f0ca3564decdb4b02bea9a012f7e2332
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-14 22:09:16 +0100

    function type

---
+++ dist/26-function-type.js
@@ -0,0 +1,6 @@
+"use strict";
+let productName;
+productName = (product) => {
+    return product;
+};
+console.log(productName("Screwdriver"));
---
+++ src/26-function-type.ts
@@ -0,0 +1,17 @@
+// function type
+
+// let productName: Function;
+
+// productName = (product: string): string => {
+//   return product;
+// };
+
+// console.log(productName("Screwdriver"));
+// ----------------------------------------------
+let productName: (product: string) => string;
+
+productName = (product) => {
+  return product;
+};
+
+console.log(productName("Screwdriver"));
