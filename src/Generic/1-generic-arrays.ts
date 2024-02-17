4ca06d97ca6bfdb31cc478e5df9005ddf62275e4
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-17 23:05:22 +0100

    generic arrays

---
+++ src/Generic/1-generic-arrays.ts
@@ -0,0 +1,10 @@
+// let num: Array<number> = [1, 2, 3];
+// let stringovi: Array<string> = ["hi", "hey"];
+
+// let regularNum: number[] = num;
+
+// let boolValues: Array<boolean> = [true, false];
+
+// let nestedArr: Array<Array<number>> = [[1, 2, 3], [2]];
+
+// let mixedArr: Array<string | number> = [1, "hi"];
