f67356411801c82596283077ead66db0d1b46b17
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-17 23:38:26 +0100

    generic object types

---
+++ dist/Generic/3-generic-object-types.js
@@ -0,0 +1,18 @@
+"use strict";
+const bookCover = {
+    front: "Book name",
+    back: "Author biography",
+};
+const bookCover2 = {
+    front: 0,
+    back: 1,
+};
+const klimaUredjaj = {
+    ime: "Klima 2024",
+    cena: 200,
+};
+const Mikrotalasna = {
+    ime: "Mikrotalasna 2024",
+    cena: "Not Available",
+};
+console.log(klimaUredjaj);
