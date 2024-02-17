b0e6b89e0e1565be7704daabe220ceb6dd027874
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-17 23:18:24 +0100

    generic functions

---
+++ src/Generic/2-generic-functions.ts
@@ -0,0 +1,13 @@
+/*  
+    the idea behind a 'generic' is that we are 
+    creating a type that could work with any data
+*/
+
+// this function return the second-indexed item of an array
+// function getArrayItem<T>(item: Array<T>): T {
+//   return item[1];
+// }
+
+// let number = getArrayItem<number>([1, 2, 3, 4, 5]);
+// let someStr = getArrayItem<string>(["Hi", "Hey", "Hello", "There"]);
+// console.log(someStr);
