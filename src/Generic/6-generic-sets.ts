ab3c9517f1559d6c90853c7a60bffb8b3ebd3a9f
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-18 15:27:34 +0100

    generic sets

---
+++ src/Generic/6-generic-sets.ts
@@ -0,0 +1,21 @@
+let mySet = new Set([1, 2, 3]);
+// sets in ts are actually generic
+
+// mySet.add('hi'); ne moze
+
+mySet.add(4);
+
+console.log(mySet);
+console.log(mySet.has(2));
+
+let mixedSet: Set<string | number> = new Set(["Hi", 1]);
+console.log(mixedSet);
+
+// let myArray: (string | number)[] = mixedSet; // ERROR
+// Set is different data structure in TypeScript
+// push, concat, shift, length, and more methods for arrays, and not for sets
+
+// let mySet2: Set<number> = [1,2,3]; // ERROR
+// add, clear, delete, has, and more methods for sets, not for arrays
+
+console.log(mixedSet.size); // same as lengths for arrays
