3f4948fc2e420767c190a11d510ef920b251e919
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-20 18:08:08 +0100

    fixes

--- src/challenges/typess/challenge-2.ts
+++ src/challenges/typess/challenge-2.ts
@@ -2,7 +2,7 @@
 What will the function invocation result in?
 */
 
-// function productId(): string | number {
+// function productsId(): string | number {
 //   return 112233;
 // }
-// let id: number = productId(); // type error (type string is not assignable to type number)
+// let id: string = productId(); // type error (type number is not assignable to type string)
