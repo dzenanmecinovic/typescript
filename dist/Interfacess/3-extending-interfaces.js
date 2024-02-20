c5a9a24479927bbbc8d70f0a6aac61ef55d34573
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-20 20:15:16 +0100

    Interfaces

--- dist/Interfacess/3-extending-interfaces.js
+++ dist/Interfacess/3-extending-interfaces.js
@@ -1,15 +1,1 @@
 "use strict";
-function informacijeZaposlenog(id, name, department, hobbies) {
-    return {
-        id,
-        name,
-        department,
-        hobbies,
-        employeeHobbies() {
-            return this.hobbies;
-        },
-    };
-}
-let radnik1 = informacijeZaposlenog(0, "Dzeno Mecinovic", ["IT"], ["GYM, Reading"]);
-console.log(radnik1);
-console.log(radnik1.employeeHobbies());
