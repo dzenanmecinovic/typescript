9f1f6392f60007b5dc9e1eaf2b87f0521319b43a
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-21 20:29:17 +0100

    nullish coalescing-operator

---
+++ src/39-nullish-coalescing-operator.ts
@@ -0,0 +1,35 @@
+// and - trazi falsy s leve na desnu i vraca je, ako su sve true vraca zadnju true
+// console.log("" && 5);
+// console.log(0 && 5);
+// console.log(false && 5);
+// console.log(null && 5);
+// console.log(undefined && 5);
+// console.log(NaN && 5);
+
+// console.log("hi" && "hey");
+// console.log("hi" && 0);
+// console.log(false && 0);
+
+// or - trazi truthy s leve na desnu i vraca je, ako su sve falsy vraca zadnju falsy
+// console.log("" || 5);
+// console.log(0 || 5);
+// console.log(false || 5);
+// console.log(null || 5);
+// console.log(undefined || 5);
+// console.log(NaN || 5);
+
+// console.log("hi" || "hey");
+// console.log("hi" || 0);
+// console.log(false || 0);
+
+// ?? - uvek vraca levu stranu operacije, sve dok leva strana nije null ili undefined
+// console.log("" ?? 5);
+// console.log(0 ?? 5);
+// console.log(false ?? 5);
+// console.log(null ?? 5);
+// console.log(undefined ?? 5);
+// console.log(NaN ?? 5);
+
+// console.log("hi" ?? "hey");
+// console.log("hi" ?? 0);
+// console.log(false ?? 0);
