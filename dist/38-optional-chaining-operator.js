1c1cbc141927f941952ebb6bdd123dbb81153126
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-21 20:19:34 +0100

    optional chaining operator

--- dist/38-optional-chaining-operator.js
+++ dist/38-optional-chaining-operator.js
@@ -1,8 +1,1 @@
 "use strict";
-const courses = [
-    { name: "JS", info: { level: "Beginner", noOfStudents: 1200 } },
-    { name: "TS", info: { level: "Beginner", noOfStudents: undefined } },
-    { name: "Tailwind CSS", info: undefined },
-];
-const courseInfo = courses.map((course) => { var _a; return (_a = course.info) === null || _a === void 0 ? void 0 : _a.noOfStudents; });
-console.log(courseInfo);
--- src/38-optional-chaining-operator.ts
+++ src/38-optional-chaining-operator.ts
@@ -1,20 +1,20 @@
-type Product = {
-  name: string;
-  info: ProductInfo | undefined;
-};
+// type Product = {
+//   name: string;
+//   info: ProductInfo | undefined;
+// };
 
-type ProductInfo = {
-  level: string;
-  noOfStudents: number | undefined;
-};
+// type ProductInfo = {
+//   level: string;
+//   noOfStudents: number | undefined;
+// };
 
-const courses: Product[] /*array of objects*/ = [
-  { name: "JS", info: { level: "Beginner", noOfStudents: 1200 } },
-  { name: "TS", info: { level: "Beginner", noOfStudents: undefined } },
-  { name: "Tailwind CSS", info: undefined },
-];
+// const courses: Product[] /*array of objects*/ = [
+//   { name: "JS", info: { level: "Beginner", noOfStudents: 1200 } },
+//   { name: "TS", info: { level: "Beginner", noOfStudents: undefined } },
+//   { name: "Tailwind CSS", info: undefined },
+// ];
 
-// console.log(courses.map((course) => course.info.noOfStudents));
+// console.log(courses);
 
 // const courseInfo = courses.map((course) => {
 //   if (course.info) return course.info.noOfStudents;
@@ -25,5 +25,5 @@ const courses: Product[] /*array of objects*/ = [
 //   return course.info ? course.info.noOfStudents : undefined;
 // });
 
-const courseInfo = courses.map((course) => course.info?.noOfStudents);
-console.log(courseInfo);
+// const courseInfo = courses.map((course) => course.info?.noOfStudents);
+// console.log(courseInfo);
