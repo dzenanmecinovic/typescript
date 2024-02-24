5c04eb1b2885b097739b4e57b226beea0b71774a
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-24 16:17:27 +0100

    oop #1

---
+++ src/OOP/creating-object-and-classes.ts
@@ -0,0 +1,26 @@
+/* Object-oriented programming revolves objects.
+Each object can contain data which is also called state. */
+
+// constructor - the name is actually self-explanatory. it is used to construct a instance of a class.
+
+// class Robot {
+//   id: number;
+//   model: string;
+//   name: string;
+
+//   constructor(id: number, model: string, name: string) {
+//     this.id = id;
+//     this.model = model;
+//     this.name = name;
+//   }
+//   walk(): void {
+//     console.log(`This robot can walk.`);
+//   }
+// }
+
+// let billyRobot = new Robot(555, "G28-K", "Billy");
+// console.log(billyRobot);
+
+// billyRobot.walk();
+
+// console.log(billyRobot instanceof Robot);
