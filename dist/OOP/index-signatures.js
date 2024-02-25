614020cb98d6ba4e8d9d81cbf3279bd259b89f71
dzenanmecinovic <mecinovicd@gmail.com>
2024-02-25 17:40:59 +0100

    oop #8

--- dist/OOP/index-signatures.js
+++ dist/OOP/index-signatures.js
@@ -1,10 +1,1 @@
 "use strict";
-class Robot {
-}
-let robot1 = new Robot();
-console.log(robot1);
-robot1.hasHands = true;
-console.log(robot1);
-let robot2 = new Robot();
-robot2.hasLegs = true;
-console.log(robot2);
